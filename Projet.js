7e0d28272180a3f404025156e324ace3247a108f
Sylvanos <syl9456@live.com>
2021-04-06 19:53:46 +0200

    Initial commit

---
+++ Projet.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "200px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0px";
+  document.getElementById("main").style.marginLeft = "0px";
+  document.body.style.backgroundColor = "#161616";
+}
