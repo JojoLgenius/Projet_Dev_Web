b0bb8c8a61d74ae4d821becd14bc9c6c2a3adb1d
Jocelyn Hauf <jocelyn.hauf@gmail.com>
2021-04-09 01:01:06 +0200

    ok2

---
+++ nextrace.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+	<head>
+		<meta charset="utf-8">
+		<title>NextRace</title>
+	</head>
+
+  <body>
+    <h1>Prochaine Course</h1>
+
+    <?php
+    $file = fopen( "textes/course.txt", "r" );
+    ?>
+
+  </body>
+</html>
