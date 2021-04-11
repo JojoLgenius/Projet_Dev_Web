012985b5212637f8d01c3c399d27da5a05f99b93
Jocelyn Hauf <jocelyn.hauf@gmail.com>
2021-04-11 15:08:57 +0200

    nextrace: Fin (Il faut ajuster le css)

--- nextrace.php
+++ nextrace.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+
 <!--Nextrace.php
 Permet d'afficher une page contenant la prochaine course
 Agit en fonction de la date actuelle
@@ -5,8 +7,6 @@ Les courses peuvent être entrées à l'avance dans un fichier courses.txt
 Le fichier course.txt doit cependant être trié au préalable
 Le fichier course.txt doit être rédigé comme le précise son utilisation
 -->
-
-<!DOCTYPE html>
 
 <html lang="fr">
 
@@ -17,8 +17,26 @@ Le fichier course.txt doit être rédigé comme le précise son utilisation
 		<script type="text/javascript" src="Projet.js"></script>
 	</head>
 
-  <body onLoad="window.setTimeout('history.go(0)', 1000)">
+	<!--
+	La page appelle chrono de Projet.js
+	Elle s'actualise toute les 5min pour éviter d'avoir un retard sur le chrono		
+	-->
+  <body onLoad="window.setTimeout('history.go(0)', 300000) ; chrono()">
+		<header>
+		<!--Icone Maison (bootsrap.com) renvoie au menu principal-->
+		<a href="Accueil.html">
+			<svg id="home"
+				xmlns="http://www.w3.org/2000/svg"
+				width="40"
+				height="40"
+				fill="#BBBBBB"
+				viewBox="0 0 15 15">
+				<path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
+			</svg>
+		</a>
 		<h1>Prochaine Course</h1>
+	</header>
+
     <?php
 		//Seconde a jours
 		function sstodd($ss){
@@ -40,7 +58,7 @@ Le fichier course.txt doit être rédigé comme le précise son utilisation
     date_default_timezone_set("Europe/Paris");
     //Ouverture du fichier
 		if($file=fopen("textes/course.txt","r")){
-		// On verifie si la ligne commence par # (->commentaires)
+		// On verifie si la ligne commence par # (commentaires)
     while(fgetc($file)=='#'){
         do{
             $c=fgetc($file);
@@ -60,11 +78,11 @@ Le fichier course.txt doit être rédigé comme le précise son utilisation
 		//On calcul la difference avec la date actuelle (exprimé en seconde)
 		$att=abs($date_course-$ajd);
 		//On affiche le temp d'atente avant la prochaine course
-		echo '<div class="attente" onLoad="chrono('.sstodd($att).','.sstohh($att).','.sstomm($att).','.restss($att).')">';
-		echo '<p class="it dd">'.sstodd($att).'J</p>
-					<p class="it hh">'.sstohh($att).'H</p>
-					<p class="it mm">'.sstomm($att).'m</p>
-					<p class="it ss">'.restss($att).'s</p>';
+		echo '<div class="attente">';
+		echo '<p class="it"><em id="dd">'.sstodd($att).'</em>J</p>
+					<p class="it"><em id="hh">'.sstohh($att).'</em>H</p>
+					<p class="it"><em id="mm">'.sstomm($att).'</em>m</p>
+					<p class="it"><em id="ss">'.restss($att).'</em>s</p>';
 		echo '</div>';
 
 		}
@@ -91,10 +109,12 @@ Le fichier course.txt doit être rédigé comme le précise son utilisation
 			<figcaption>Source: <em class="source">Wikipedia</em></figcaption>
 		</figure>
 		</div>
+
+		<footer>
+			<?php
+				echo '<p class="date_actuel">Date : '.utf8_encode(date('d/m/Y')).'</p>';
+			 ?>
+		</footer>
   </body>
-	<footer>
-		<?php
-			echo '<p class="date_actuel">'.utf8_encode(date('d/m/Y H:i:s')).'</p>';
-		 ?>
-	</footer>
+
 </html>
