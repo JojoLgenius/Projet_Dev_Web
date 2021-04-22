d84e8695fa1ac6a5c2e91ae2a4734c03d918c4ba
Sylvanos <37038164+syl9456@users.noreply.github.com>
2021-04-22 22:34:42 +0200

    Update nextrace.php

--- nextrace.php
+++ nextrace.php
@@ -24,7 +24,7 @@ Le fichier course.txt doit être rédigé comme le précise son utilisation
   <body onLoad="window.setTimeout('history.go(0)', 300000) ; chrono()">
 		<header>
 		<!--Icone Maison (bootsrap.com) renvoie au menu principal-->
-		<a href="Accueil.html">
+		<a href="Accueil.php">
 			<svg id="home"
 				xmlns="http://www.w3.org/2000/svg"
 				width="40"
