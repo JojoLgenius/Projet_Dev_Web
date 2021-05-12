4059d1b5b5740e46738040ac92bc1b34c4d4a50f
Sylvanos <37038164+syl9456@users.noreply.github.com>
2021-05-12 18:32:57 +0200

    Add files via upload

--- Webetu/Contact.php
+++ Webetu/Contact.php
@@ -121,7 +121,7 @@ session_start();
 				</div>
 				<div id="card1">
 					<div class="card">
-  						<img src="images/imgPortrait1.jpg" alt="Sylvain" style="width:50%">
+  						<img src="images/Createur/Sylvain.jpg" alt="Sylvain" style="width:50%">
   						<h1>Sylvain Rome</h1>
   						<p class="title">Etudiant L2</p>
   						<p>Jean Monnet  Saint-Etienne</p>
